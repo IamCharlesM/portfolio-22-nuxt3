4719a424d213ab29e1856157951099c664643f20
IamCharlesM <rafroyevans@gmail.com>
2022-11-26 11:28:33 -0600

    Basic page created

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+      ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+    },
+  },
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "width=device-width, height=device-height, initial-scale=1,",
+      title: "TESTING",
+      meta: [{ name: "description", content: "pls" }],
+    },
+  },
+});
