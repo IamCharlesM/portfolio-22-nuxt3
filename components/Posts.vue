04e9686eacb0893b5cd3564a9578371cafba0aec
IamCharlesM <rafroyevans@gmail.com>
2022-12-19 03:56:34 -0600

    client only

--- components/Posts.vue
+++ components/Posts.vue
@@ -13,35 +13,37 @@
       :key="post.id"
       class="card min-w-fit bg-base-100 shadow-xl"
     >
-      <NuxtLink :to="'project/' + post.slug">
-        <figure>
-          <!--  ANCHOR image and alt text -->
-          <img
-            class="h-60"
-            :src="post.feature_image"
-            :alt="post.feature_image_alt"
-          />
-        </figure>
-        <div class="card-body">
-          <!-- ANCHOR Post title -->
-          <h2 class="card-title text-white">
-            {{ post.title }}
-            <div class="badge text-yellow-600">NEW</div>
-          </h2>
-          <p class="text-white">{{ post.excerpt }}</p>
-          <div class="card-actions justify-end">
-            <div v-for="tag in post.tags" :key="tag.id">
-              <div
-                v-if="tag.visibility === 'public'"
-                class="badge badge-outline text-yellow-600"
-              >
-                {{ tag.name }}
+      <client-only>
+        <NuxtLink :to="'project/' + post.slug">
+          <figure>
+            <!--  ANCHOR image and alt text -->
+            <img
+              class="h-60"
+              :src="post.feature_image"
+              :alt="post.feature_image_alt"
+            />
+          </figure>
+          <div class="card-body">
+            <!-- ANCHOR Post title -->
+            <h2 class="card-title text-white">
+              {{ post.title }}
+              <div class="badge text-yellow-600">NEW</div>
+            </h2>
+            <p class="text-white">{{ post.excerpt }}</p>
+            <div class="card-actions justify-end">
+              <div v-for="tag in post.tags" :key="tag.id">
+                <div
+                  v-if="tag.visibility === 'public'"
+                  class="badge badge-outline text-yellow-600"
+                >
+                  {{ tag.name }}
+                </div>
+                <span v-else class="hidden"></span>
               </div>
-              <span v-else class="hidden"></span>
             </div>
           </div>
-        </div>
-      </NuxtLink>
+        </NuxtLink>
+      </client-only>
     </div>
   </div>
 </template>
