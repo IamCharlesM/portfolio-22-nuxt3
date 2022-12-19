e99bd4ee1dfca41bc447b3d9ebcf7eef30eeba32
IamCharlesM <rafroyevans@gmail.com>
2022-12-19 01:29:57 -0600

    Dynamically create pages based on slug

--- components/Posts.vue
+++ components/Posts.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="pending">
+    <div v-if="pending" class="flex flex-row space-x-5">
       <!-- Display an empty div if data is null -->
       <ImageLoader class="" />
       <ImageLoader class="" />
@@ -13,30 +13,35 @@
       :key="post.id"
       class="card min-w-fit bg-base-100 shadow-xl"
     >
-      <figure>
-        <!--  ANCHOR image and alt text -->
-        <img
-          class="h-48"
-          :src="post.feature_image"
-          :alt="post.feature_image_alt"
-        />
-      </figure>
-      <div class="card-body">
-        <!-- ANCHOR Post title -->
-        <h2 class="card-title">
-          {{ post.title }}
-          <div class="badge">NEW</div>
-        </h2>
-        <p>{{ post.excerpt }}</p>
-        <div class="card-actions justify-end">
-          <div v-for="tag in post.tags" :key="tag.id">
-            <div v-if="tag.visibility === 'public'" class="badge badge-outline">
-              {{ tag.name }}
+      <NuxtLink :to="'project/' + post.slug">
+        <figure>
+          <!--  ANCHOR image and alt text -->
+          <img
+            class="h-60"
+            :src="post.feature_image"
+            :alt="post.feature_image_alt"
+          />
+        </figure>
+        <div class="card-body">
+          <!-- ANCHOR Post title -->
+          <h2 class="card-title text-white">
+            {{ post.title }}
+            <div class="badge text-yellow-600">NEW</div>
+          </h2>
+          <p class="text-white">{{ post.excerpt }}</p>
+          <div class="card-actions justify-end">
+            <div v-for="tag in post.tags" :key="tag.id">
+              <div
+                v-if="tag.visibility === 'public'"
+                class="badge badge-outline text-yellow-600"
+              >
+                {{ tag.name }}
+              </div>
+              <span v-else class="hidden"></span>
             </div>
-            <span v-else class="hidden"></span>
           </div>
         </div>
-      </div>
+      </NuxtLink>
     </div>
   </div>
 </template>
--- layouts/project.vue
+++ layouts/project.vue
@@ -3,6 +3,6 @@
     class="hero min-h-screen max-w-screen bg-gradient-to-br from-slate-800 via-slate-900 to-black flex flex-col"
   >
     <Navbar class="mb-10" />
-    <slot />
+    <slot class="justify-items-center" />
   </div>
 </template>
