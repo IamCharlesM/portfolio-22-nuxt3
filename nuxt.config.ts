63c06172723bb017d9ec731fd7b00847b353a726
IamCharlesM <rafroyevans@gmail.com>
2022-12-22 21:14:41 -0600

    Add robots.txt and sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", '@nuxt/content', "@tailwindcss/typography"],
+  modules: ["@nuxtjs/tailwindcss", '@nuxt/content', "@tailwindcss/typography", '@nuxtjs/robots'],
   postcss: {
     plugins: {
       autoprefixer: {},
