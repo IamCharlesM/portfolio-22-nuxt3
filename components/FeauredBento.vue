bf39bca6b85cd83509bbce30cb04d4795dfc4bcd
IamCharlesM <because@charlesisa.dev>
2024-05-20 00:01:18 -0500

    Featured bento created

---
+++ components/FeauredBento.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="bg-red-600 h-full flex md:w-1/2 rounded-3xl bg-cover bg-center"
+    :style="{ 'background-image': `url(${image})` }"
+  >
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque ab
+    dignissimos molestiae, quis repellat eum quia vel commodi et reprehenderit
+    suscipit provident? Deleniti ipsa reprehenderit dolore expedita! Quibusdam,
+    sint fugit!
+  </div>
+</template>
+
+<script setup>
+defineProps(["image"]);
+</script>
