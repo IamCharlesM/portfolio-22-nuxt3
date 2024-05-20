3f2178526b64ab2438e78c97cb01919ab90f8784
IamCharlesM <because@charlesisa.dev>
2024-05-19 21:35:20 -0500

    Add navbar.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -26,17 +26,29 @@
           <li v-for="(page, index) in pages" :key="index">
             <nuxtLink :to="page.to" class="">{{ page.property }}</nuxtLink>
           </li>
+          <li>
+            <a
+              href="https://calendly.com/d/44j-vwq-mtv/30-minute-meeting"
+              target="_blank"
+              class=""
+              >Work with me</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://docs.google.com/document/d/1iU0btnTA44hugqX1iixqxUWb-Nl92XZbhwmcqvL32aQ/edit#heading=h.rlsx4o5b4mpo"
+              target="_blank"
+              class=""
+              >Resume</a
+            >
+          </li>
         </ul>
       </div>
     </div>
     <!-- ANCHOR Desktop items -->
     <div class="navbar-center hidden lg:flex">
-      <ul
-        class="menu menu-horizontal px-1"
-        v-for="(page, index) in pages"
-        :key="index"
-      >
-        <li>
+      <ul class="menu menu-horizontal px-1">
+        <li v-for="(page, index) in pages" :key="index">
           <nuxtLink :to="page.to" class="">{{ page.property }}</nuxtLink>
         </li>
         <li>
