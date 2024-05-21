2953b8647be6df6d6d4c5bcd177a54697281965c
IamCharlesM <because@charlesisa.dev>
2024-05-21 14:14:17 -0500

    refactor: Update project layout and components

--- components/FeauredBento.vue
+++ components/FeauredBento.vue
@@ -1,20 +1,20 @@
 <template>
   <div
-    class="bg-red-600 h-full flex md:w-1/2 rounded-3xl bg-cover bg-center p-4"
+    class="bg-base-300 rounded-3xl bg-cover bg-center p-4 col-span-full md:col-span-4 row-span-2 md:row-span-3"
   >
     <div v-if="title == 'Projects'">
-      <h2 class="text-white text-4xl w-3/4 font-bold">
+      <h2 class="text-white text-xl md:text-4xl font-bold">
         Explore my technical achievements, from coding solutions to software
         applications.
       </h2>
       <button class="btn btn-square btn-lg">text</button>
     </div>
     <div v-else>
-      <h2 class="text-white text-4xl w-3/4 font-bold">
+      <h2 class="text-white text-xl md:text-4xl font-bold">
         Read the stories behind my projects, highlighting challenges,
         breakthroughs, and lessons learned.
       </h2>
-      <button class="btn btn-square btn-lg">text</button>
+      <button class="btn btn-square md:btn-lg">text</button>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,16 @@
 <template>
-  <div class="hero min-h-screen max-w-screen overflow-x-clip">
-    <div class="w-screen min-h-screen my-5">
-      <div class="mx-5 lg:mx-20">
-        <!-- ANCHOR Main content -->
-        <HeroSection />
-        <!-- ANCHOR Skills loop -->
-        <ContentSection type="front-end-development" title="Projects" />
-        <ContentSection type="hash-projects" title="Stories" />
-        <div class="flex flex-col space-y-5 overflow-auto">
-          <div class="flex flex-row space-x-5">
-            <p class="text-yellow-600">// Most recent posts</p>
-            <span class="text-yellow-600 lg:hidden">Swipe 👉🏿</span>
-          </div>
-
-          <Posts
-            class="flex flex-row space-x-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-2"
-          />
-        </div>
-      </div>
+  <div class="w-full px-4 md:">
+    <div>
+      <!-- ANCHOR Main content -->
+      <HeroSection />
+      <!-- ANCHOR Skills loop -->
+      <ContentSection type="front-end-development" title="Projects" />
+      <ContentSection type="hash-projects" title="Stories" :alt="true" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { Icon } from "@iconify/vue";
-
 definePageMeta({ layout: "project" });
 
 const { data, pending } = useFetch(
