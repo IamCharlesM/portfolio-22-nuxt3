1f9210aab6fda0891c94307453f079240b13be6a
IamCharlesM <because@charlesisa.dev>
2024-05-21 21:57:42 -0500

    Grid changes

--- layouts/project.vue
+++ layouts/project.vue
@@ -1,8 +1,6 @@
 <template>
-  <div
-    class="hero min-h-screen max-w-screen bg-base-300 flex flex-col text-neutral"
-  >
-    <Navbar class="bg-base-300" />
-    <slot class="justify-items-center" />
+  <div class="hero min-h-screen bg-base-100 flex flex-col text-neutral">
+    <Navbar />
+    <slot />
   </div>
 </template>
